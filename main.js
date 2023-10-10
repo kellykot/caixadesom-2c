fccaf31565807f21c41f7fc4231f5a4c168ac155
kellykot <111991009+kellykot@users.noreply.github.com>
2023-10-10 09:12:21 -0300

    1

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function tocaSomPom(){
     document.querySelector('#son_tecla_pom').play()
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#son_tecla_clap').play()
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
