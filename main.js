f50809418cf70945afbafc4b876e6d7c75b3c0db
kellykot <111991009+kellykot@users.noreply.github.com>
2023-10-17 09:16:34 -0300

    ela

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+//declaração/criando função tocaSomPom
 function tocaSomPom(){
-    document.querySelector('#son_tecla_pom').play()
+    document.querySelector('#son_tecla_pom').play();
 }
+
+//chamando/invocando a função tocaSomPom pelo click do botão
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap(){
-    document.querySelector('#son_tecla_clap').play()
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[1].onclick = TocaSomClap;
+//referencia constante listaDeTeclas, buscandotodos os botões
+
+//chamando a função tocaSomPom pelo item "0" da listaDeTecla
+listaDeTecklas[0].onclick = tocaSomPom
