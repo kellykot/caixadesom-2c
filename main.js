ce676ab4439ffe4f87ab3782ffc9c58b60c05c63
kellykot <111991009+kellykot@users.noreply.github.com>
2023-10-24 09:23:04 -0300

    vc fh gnbbn

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ listaDeTeclas[1].onclick = tocaSomClap;
 //referencia constante listaDeTeclas, buscandotodos os botões
 
 //chamando a função tocaSomPom pelo item "0" da listaDeTecla
-listaDeTeclas[0].onclick = tocaSomPom
+listaDeTeclas[0].onclick = tocaSomPom;
 
 let contador = 0;
-while(contador < 9){
+while(contador < listaDeTeclas.length){
 listaDeTeclas[contador].onclick =tocaSomPom;
 contador = contador + 1;
 console.log(contador);
