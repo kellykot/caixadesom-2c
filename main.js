e239c0a0ba94c3ba4d19334360e5f5e30650e694
kellykot <111991009+kellykot@users.noreply.github.com>
2023-10-24 09:16:51 -0300

    somos lindas

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 //declaração/criando função tocaSomPom
 function tocaSomPom(){
-    document.querySelector('#son_tecla_pom').play();
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
 }
 
 //chamando/invocando a função tocaSomPom pelo click do botão
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[1].onclick = TocaSomClap;
+listaDeTeclas[1].onclick = tocaSomClap;
 //referencia constante listaDeTeclas, buscandotodos os botões
 
 //chamando a função tocaSomPom pelo item "0" da listaDeTecla
